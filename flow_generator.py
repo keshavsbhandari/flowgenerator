44ccb4379b6cefab02e339e919975baca5d34489
Keshav Bhandari <34430028+keshavsbhandari@users.noreply.github.com>
2021-07-29 23:42:38 -0500

    Update flow_generator.py

--- flow_generator.py
+++ flow_generator.py
@@ -532,7 +532,8 @@ class NODE_OT_TEST(bpy.types.Operator):
 class ADD_TO_VIEW_PORT(NODE_PT_MAINPANEL):
     bl_space_type = 'VIEW_3D'
    
-classes = [MyProperties, CAMERA_SETUP, NODE_PT_MAINPANEL, ADD_TO_VIEW_PORT, NODE_OT_TEST]   
+#classes = [MyProperties, CAMERA_SETUP, NODE_PT_MAINPANEL, ADD_TO_VIEW_PORT, NODE_OT_TEST]
+classes = [MyProperties, CAMERA_SETUP, ADD_TO_VIEW_PORT, NODE_OT_TEST]   
     
 def register():
     for cl in classes:
